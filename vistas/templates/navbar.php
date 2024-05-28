cb19a67129b3b4f19b98f4137103c05ba62f8066
calo13 <carlologo96@gmail.com>
2024-05-28 09:24:34 -0600

    FORMULARIO DE CLIENTES

--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -18,6 +18,15 @@
             <li><a class="dropdown-item" href="/crud_2024/vistas/producto/buscar.php"><i class="bi bi-search me-2"></i>Buscar</a></li>
           </ul>
         </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-people me-2"></i>Clientes
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/crud_2024/vistas/cliente/index.php"><i class="bi bi-plus-circle me-2"></i>Crear</a></li>
+            <li><a class="dropdown-item" href="/crud_2024/vistas/cliente/buscar.php"><i class="bi bi-search me-2"></i>Buscar</a></li>
+          </ul>
+        </li>
     </div>
   </div>
 </nav>
